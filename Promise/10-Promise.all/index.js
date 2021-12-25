79deab71269cb36dffb50055e9c043a45a5c427c
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2021-12-25 16:04:08 +0800

    async-await wit promises

--- Promise/10-Promise.all/index.js
+++ Promise/10-Promise.all/index.js
@@ -15,30 +15,30 @@ function queryAPI(endpoint) {
 // without Promise.all()
 // Promises will resolve in waterfall manner (one after another)
 
-// queryAPI("films")
-//   .then((films) => {
-//     return queryAPI("planets").then((planets) => {
-//       output.innerText = `${films.length} films ,${planets.length} planets`;
-//     });
-//   })
-//   .finally(() => {
-//     spinner.remove();
-//   });
+queryAPI("films")
+  .then((films) => {
+    return queryAPI("planets").then((planets) => {
+      output.innerText = `${films.length} films ,${planets.length} planets`;
+    });
+  })
+  .finally(() => {
+    spinner.remove();
+  });
 
-// const promiseList = Promise.all([queryAPI("films"), queryAPI("planets")]); // multiple promise calls
-// // Promise.all will be rejected if any promise is rejected
-// promiseList
-//   .then((result) => {
-//     // result will contain according to given order above
-//     console.log(result[0].length + " films, ", result[1].length + " planets");
-//     output.innerText = `${result[0].length} films ,${result[1].length} planets`;
-//   })
-//   .catch((err) => {
-//     console.warn("Error: " + err);
-//   })
-//   .finally(() => {
-//     spinner.remove();
-//   });
+const promiseList = Promise.all([queryAPI("films"), queryAPI("planets")]); // multiple promise calls
+// Promise.all will be rejected if any promise is rejected
+promiseList
+  .then((result) => {
+    // result will contain according to given order above
+    console.log(result[0].length + " films, ", result[1].length + " planets");
+    output.innerText = `${result[0].length} films ,${result[1].length} planets`;
+  })
+  .catch((err) => {
+    console.warn("Error: " + err);
+  })
+  .finally(() => {
+    spinner.remove();
+  });
 
 Promise.allSettled([queryAPI("films"), queryAPI("planets"), queryAPI("movies")]) // will return settltment objects
   .then((result) => {
@@ -55,4 +55,5 @@ Promise.allSettled([queryAPI("films"), queryAPI("planets"), queryAPI("movies")])
   });
 
 
-  // Promise.any() returns the first promise fulfilled
+  // Promise.any() - returns the first promise fulfilled
+  // Promise.allSettled() - 
