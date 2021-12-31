e03eb80cc7b10d342702caf6bf1b89d9a60df93f
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2021-12-31 13:01:42 +0800

    event bubbling example

--- event-bubbling/app.js
+++ event-bubbling/app.js
@@ -3,6 +3,7 @@ const ul = document.querySelector("ul");
 const li = document.querySelector("li");
 const button = document.querySelector("input");
 const wrapper = document.querySelector(".wrapper");
+const containerBubble = document.querySelector("#container-bubble");
 
 // due to event bubblig, once you click li, click events for ul and container will also get executed.
 li.addEventListener("click", () => console.log("li - Clicked :)"));
@@ -23,4 +24,12 @@ wrapper.addEventListener(
   true
 ); // passing third argument as true will enable event capturing
 
-// for list of elements, instead of adding click events to all, you can add single click event to parent and get value from e.target and handle the click.
+/** 
+ * usecase
+  for list of elements, instead of adding click events to all, 
+  you can add single click event to parent and get value from e.target and handle the click.
+*/
+
+containerBubble.addEventListener('click', (e) => {
+  console.log('clicked list item: ', e.target)
+})
